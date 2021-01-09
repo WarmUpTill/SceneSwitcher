0e3da3782fe54bed37b51d79cc30bae339ff7622
WarmUpTill <WarmUpTill@web.de>
2021-01-10 00:33:38 +0100

    add draft of scene groups
    The idea is to allow specifying a group of scenes as a target for a switch entry.
    The next active scene can be specified based on a variety of conditions (time, count, random).
    This should allow configuring even more elaborate switching setups.

---
+++ src/headers/scene-switch-target.hpp
@@ -0,0 +1,51 @@
+#include <vector>
+#include <QComboBox>
+#include <obs.hpp>
+
+enum class AdvanceCondition {
+	Count,
+	Time,
+	Random,
+};
+
+class SceneGroup {
+	void AddScene();
+	void RemoveScene();
+	void UpdateCount();
+	void UpdateTime();
+
+	void getNextScene();
+
+private:
+	AdvanceCondition condition;
+	std::vector<OBSWeakSource> scenes;
+};
+
+enum class SwitchTargetType {
+	Scene,
+	SceneGroup,
+};
+
+struct SwitchTarget {
+	SwitchTargetType type;
+	union {
+		OBSWeakSource scene;
+		SceneGroup group;
+	};
+};
+
+class SceneGroupWidget {
+	Q_OBJECT
+
+public:
+	SceneGroupWidget(SceneGroup *s);
+private slots:
+	void ConditionChanged(int cond);
+	//...
+
+private:
+	QComboBox *scenes;
+	QComboBox *condition;
+
+	SceneGroup *group;
+};
---
+++ src/scene-switch-target.cpp
@@ -0,0 +1,11 @@
+#include "headers\scene-switch-target.hpp"
+
+void SceneGroup::AddScene() {}
+
+void SceneGroup::RemoveScene() {}
+
+void SceneGroup::UpdateCount() {}
+
+void SceneGroup::UpdateTime() {}
+
+void SceneGroup::getNextScene() {}
