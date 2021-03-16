cdfafb321f8d11b603fd258dec12f673b8cdce8a
WarmUpTill <WarmUpTill@web.de>
2021-03-16 19:28:48 +0100

    Add some sample code based on obs-websockets

--- src/headers/switch-network.hpp
+++ src/headers/switch-network.hpp
@@ -1,4 +1,39 @@
 #pragma once
-#include <asio.hpp>
 #include <websocketpp/config/asio_no_tls.hpp>
 #include <websocketpp/server.hpp>
+
+
+using websocketpp::connection_hdl;
+
+typedef websocketpp::server<websocketpp::config::asio> server;
+
+class WSServer : public QObject {
+	Q_OBJECT
+
+public:
+	explicit WSServer();
+	//virtual ~WSServer();
+	//void start(quint16 port, bool lockToIPv4);
+	//void stop();
+	//void broadcast(const RpcEvent &event);
+	//QThreadPool *threadPool() { return &_threadPool; }
+
+private:
+	//void onOpen(connection_hdl hdl);
+	//void onMessage(connection_hdl hdl, server::message_ptr message);
+	//void onClose(connection_hdl hdl);
+	//
+	//QString getRemoteEndpoint(connection_hdl hdl);
+	//void notifyConnection(QString clientIp);
+	//void notifyDisconnection(QString clientIp);
+
+	server _server;
+	quint16 _serverPort;
+	//bool _lockToIPv4;
+	//std::set<connection_hdl, std::owner_less<connection_hdl>> _connections;
+	//std::map<connection_hdl, ConnectionProperties,
+	//	 std::owner_less<connection_hdl>>
+	//	_connectionProperties;
+	//QMutex _clMutex;
+	//QThreadPool _threadPool;
+};
