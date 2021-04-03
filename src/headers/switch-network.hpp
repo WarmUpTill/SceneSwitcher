177788a640bc15960ae96bbe703bc4beca32f5a5
WarmUpTill <WarmUpTill@web.de>
2021-04-03 14:34:30 +0200

    Adjust include order ...

--- src/headers/switch-network.hpp
+++ src/headers/switch-network.hpp
@@ -12,9 +12,10 @@ Most of this code is based on https://github.com/Palakis/obs-websocket
 #include <QtCore/QVariantHash>
 #include <QtCore/QThreadPool>
 
-#include <websocketpp/config/asio_no_tls_client.hpp>
 #include <websocketpp/config/asio_no_tls.hpp>
 #include <websocketpp/server.hpp>
+
+#include <websocketpp/config/asio_no_tls_client.hpp>
 #include <websocketpp/client.hpp>
 
 #include <mutex>
