0f1dfa153178257483c9ec5bb77db5eb58289cc5
WarmUpTill <WarmUpTill@web.de>
2021-04-17 13:57:35 +0200

    add draft of window condition

---
+++ src/headers/macro-condition-window.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include "macro-entry.hpp"
+#include <QWidget>
+#include <QComboBox>
+
+class MacroConditionWindow : public MacroCondition {
+public:
+	bool CheckCondition();
+	bool Save();
+	bool Load();
+	static std::shared_ptr<MacroCondition> Create()
+	{
+		return std::make_shared<MacroConditionWindow>();
+	}
+
+	std::string _window;
+
+private:
+	static bool _registered;
+};
+
+class MacroConditionWindowEdit : public QWidget {
+	Q_OBJECT
+
+public:
+	MacroConditionWindowEdit(
+		std::shared_ptr<MacroConditionWindow> cond = nullptr);
+	void UpdateEntryData();
+	static QWidget *Create(std::shared_ptr<MacroCondition> cond)
+	{
+		return new MacroConditionWindowEdit(
+			std::dynamic_pointer_cast<MacroConditionWindow>(cond));
+	}
+
+private slots:
+	void WindowChanged(const QString &text);
+
+protected:
+	QComboBox *_windowSelection;
+	std::shared_ptr<MacroConditionWindow> _entryData;
+
+private:
+	bool _loading = true;
+};
---
+++ src/macro-condition-window.cpp
@@ -0,0 +1,70 @@
+#include "headers/macro-condition-edit.hpp"
+#include "headers/macro-condition-window.hpp"
+#include "headers/utility.hpp"
+#include "headers/advanced-scene-switcher.hpp"
+
+bool MacroConditionWindow::_registered = MacroConditionFactory::Register(
+	1, {MacroConditionWindow::Create, MacroConditionWindowEdit::Create,
+	    "AdvSceneSwitcher.condition.window"});
+
+bool MacroConditionWindow::CheckCondition()
+{
+	std::string currentWindow;
+
+	GetCurrentWindowTitle(currentWindow);
+	return currentWindow == _window;
+}
+
+bool MacroConditionWindow::Save()
+{
+	return false;
+}
+
+bool MacroConditionWindow::Load()
+{
+	return false;
+}
+
+MacroConditionWindowEdit::MacroConditionWindowEdit(
+	std::shared_ptr<MacroConditionWindow> entryData)
+{
+	_windowSelection = new QComboBox();
+
+	QWidget::connect(_windowSelection,
+			 SIGNAL(currentTextChanged(const QString &)), this,
+			 SLOT(WindowChanged(int)));
+
+	AdvSceneSwitcher::populateWindowSelection(_windowSelection);
+
+	QHBoxLayout *mainLayout = new QHBoxLayout;
+	std::unordered_map<std::string, QWidget *> widgetPlaceholders = {
+		{"{{windows}}", _windowSelection},
+	};
+	placeWidgets(obs_module_text(
+			     "AdvSceneSwitcher.macro.condition.window.entry"),
+		     mainLayout, widgetPlaceholders);
+	setLayout(mainLayout);
+
+	_entryData = entryData;
+	UpdateEntryData();
+	_loading = false;
+}
+
+void MacroConditionWindowEdit::WindowChanged(const QString &text)
+{
+	if (_loading || !_entryData) {
+		return;
+	}
+
+	std::lock_guard<std::mutex> lock(switcher->m);
+	_entryData->_window = text.toStdString();
+}
+
+void MacroConditionWindowEdit::UpdateEntryData()
+{
+	if (!_entryData) {
+		return;
+	}
+
+	_windowSelection->setCurrentText(_entryData->_window.c_str());
+}
