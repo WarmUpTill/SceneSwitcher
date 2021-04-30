505384048352f729c0547d6f37c0d7b786feaa2c
WarmUpTill <WarmUpTill@web.de>
2021-04-30 23:38:32 +0200

    Move compareIgnoringLineEnding() to utility.hpp

--- src/headers/utility.hpp
+++ src/headers/utility.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QString>
+#include <QTextStream>
 #include <QLayout>
 #include <QLabel>
 #include <unordered_map>
@@ -181,4 +182,25 @@ placeWidgets(std::string text, QBoxLayout *layout,
 	if (addStretch) {
 		layout->addStretch();
 	}
+}
+
+static inline bool compareIgnoringLineEnding(QString &s1, QString &s2)
+{
+	// Let QT deal with different types of lineendings
+	QTextStream s1stream(&s1);
+	QTextStream s2stream(&s2);
+
+	while (!s1stream.atEnd() || !s2stream.atEnd()) {
+		QString s1s = s1stream.readLine();
+		QString s2s = s2stream.readLine();
+		if (s1s != s2s) {
+			return false;
+		}
+	}
+
+	if (!s1stream.atEnd() && !s2stream.atEnd()) {
+		return false;
+	}
+
+	return true;
 }
