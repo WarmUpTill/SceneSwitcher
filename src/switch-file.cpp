2ed65fa893539d3e5e4b44d594e415af706a6537
WarmUpTill <WarmUpTill@web.de>
2021-04-30 23:33:13 +0200

    Move compareIgnoringLineEnding to utility.hpp

--- src/switch-file.cpp
+++ src/switch-file.cpp
@@ -161,7 +161,7 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb,
 	return size * nmemb;
 }
 
-std::string getRemoteData(std::string &url)
+static std::string getRemoteData(std::string &url)
 {
 	std::string readBuffer;
 
@@ -173,32 +173,6 @@ std::string getRemoteData(std::string &url)
 		f_curl_perform(switcher->curl);
 	}
 	return readBuffer;
-}
-
-bool compareIgnoringLineEnding(QString &s1, QString &s2)
-{
-	/*
-	Im using QTextStream here so the conversion between different lineendings is done by QT.
-	QT itself uses only the linefeed internally so the input by the user is always using that,
-	but the files selected by the user might use different line endings.
-	If you are reading this and know of a cleaner way to do this, please let me know :)
-	*/
-	QTextStream s1stream(&s1);
-	QTextStream s2stream(&s2);
-
-	while (!s1stream.atEnd() || !s2stream.atEnd()) {
-		QString s1s = s1stream.readLine();
-		QString s2s = s2stream.readLine();
-		if (s1s != s2s) {
-			return false;
-		}
-	}
-
-	if (!s1stream.atEnd() && !s2stream.atEnd()) {
-		return false;
-	}
-
-	return true;
 }
 
 bool matchFileContent(QString &filedata, FileSwitch &s)
