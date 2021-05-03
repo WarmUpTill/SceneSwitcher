131413642c93195c3ae602f5c41cddd12549ed65
WarmUpTill <WarmUpTill@web.de>
2021-05-03 22:01:56 +0200

    Add video macro condition

---
+++ src/headers/macro-condition-video.hpp
@@ -0,0 +1,82 @@
+#pragma once
+#include "macro.hpp"
+#include <QWidget>
+#include <QComboBox>
+#include <chrono>
+#include "duration-control.hpp"
+#include "screenshot-helper.hpp"
+
+enum class VideoCondition {
+	MATCH,
+	DIFFER,
+	HAS_NOT_CHANGED,
+	HAS_CHANGED,
+	NO_IMAGE,
+};
+
+class MacroConditionVideo : public MacroCondition {
+public:
+	bool CheckCondition();
+	bool Save(obs_data_t *obj);
+	bool Load(obs_data_t *obj);
+	int GetId() { return id; };
+	QImage GetMatchImage() { return _matchImage; };
+	static std::shared_ptr<MacroCondition> Create()
+	{
+		return std::make_shared<MacroConditionVideo>();
+	}
+	void GetScreenshot();
+	bool LoadImageFromFile();
+
+	OBSWeakSource _videoSource;
+	VideoCondition _condition = VideoCondition::MATCH;
+	Duration _duration;
+	std::string _file = obs_module_text("AdvSceneSwitcher.enterPath");
+
+private:
+	bool Compare();
+
+	std::unique_ptr<AdvSSScreenshotObj> _screenshotData = nullptr;
+	QImage _matchImage;
+	static bool _registered;
+	static const int id;
+};
+
+class MacroConditionVideoEdit : public QWidget {
+	Q_OBJECT
+
+public:
+	MacroConditionVideoEdit(
+		QWidget *parent,
+		std::shared_ptr<MacroConditionVideo> cond = nullptr);
+	void UpdateEntryData();
+	static QWidget *Create(QWidget *parent,
+			       std::shared_ptr<MacroCondition> cond)
+	{
+		return new MacroConditionVideoEdit(
+			parent,
+			std::dynamic_pointer_cast<MacroConditionVideo>(cond));
+	}
+
+	void UpdatePreviewTooltip();
+	void SetFilePath(const QString &text);
+
+private slots:
+	void SourceChanged(const QString &text);
+	void ConditionChanged(int cond);
+	void FilePathChanged();
+	void BrowseButtonClicked();
+	void DurationChanged(double seconds);
+	void DurationUnitChanged(DurationUnit unit);
+
+protected:
+	QComboBox *_videoSelection;
+	DurationSelection *_duration;
+	QComboBox *_condition;
+	QLineEdit *_filePath;
+	QPushButton *_browseButton;
+	std::shared_ptr<MacroConditionVideo> _entryData;
+
+private:
+	bool _loading = true;
+};
---
+++ src/macro-condition-video.cpp
@@ -0,0 +1,359 @@
+#include "headers/macro-condition-edit.hpp"
+#include "headers/macro-condition-video.hpp"
+#include "headers/utility.hpp"
+#include "headers/advanced-scene-switcher.hpp"
+
+#include <QFileDialog>
+#include <QBuffer>
+#include <QToolTip>
+#include <QMessageBox>
+
+const int MacroConditionVideo::id = 6;
+
+bool MacroConditionVideo::_registered = MacroConditionFactory::Register(
+	MacroConditionVideo::id,
+	{MacroConditionVideo::Create, MacroConditionVideoEdit::Create,
+	 "AdvSceneSwitcher.condition.video"});
+
+static std::unordered_map<VideoCondition, std::string> conditionTypes = {
+	{VideoCondition::MATCH,
+	 "AdvSceneSwitcher.condition.video.condition.match"},
+	{VideoCondition::DIFFER,
+	 "AdvSceneSwitcher.condition.video.condition.differ"},
+	{VideoCondition::HAS_NOT_CHANGED,
+	 "AdvSceneSwitcher.condition.video.condition.hasNotChanged"},
+	{VideoCondition::HAS_CHANGED,
+	 "AdvSceneSwitcher.condition.video.condition.hasChanged"},
+	{VideoCondition::NO_IMAGE,
+	 "AdvSceneSwitcher.condition.video.condition.noImage"},
+};
+
+bool requiresFileInput(VideoCondition t)
+{
+	return t == VideoCondition::MATCH || t == VideoCondition::DIFFER;
+}
+
+bool MacroConditionVideo::CheckCondition()
+{
+	bool match = false;
+
+	if (_screenshotData) {
+		if (_screenshotData->done) {
+			bool imageMatch = Compare();
+
+			if (!imageMatch) {
+				_duration.Reset();
+			}
+
+			if (imageMatch && _duration.DurationReached()) {
+				match = true;
+			}
+
+			if (!requiresFileInput(_condition)) {
+				_matchImage = std::move(_screenshotData->image);
+			}
+			_screenshotData.reset(nullptr);
+		}
+	}
+
+	GetScreenshot();
+	return match;
+}
+
+bool MacroConditionVideo::Save(obs_data_t *obj)
+{
+	MacroCondition::Save(obj);
+	obs_data_set_string(obj, "videoSource",
+			    GetWeakSourceName(_videoSource).c_str());
+	obs_data_set_int(obj, "condition", static_cast<int>(_condition));
+	_duration.Save(obj);
+	obs_data_set_string(obj, "filePath", _file.c_str());
+	return true;
+}
+
+bool MacroConditionVideo::Load(obs_data_t *obj)
+{
+	const char *videoSourceName = obs_data_get_string(obj, "videoSource");
+	_videoSource = GetWeakSourceByName(videoSourceName);
+	_condition =
+		static_cast<VideoCondition>(obs_data_get_int(obj, "condition"));
+	_duration.Load(obj);
+	_file = obs_data_get_string(obj, "filePath");
+
+	if (requiresFileInput(_condition)) {
+		(void)LoadImageFromFile();
+	}
+	return true;
+}
+
+void MacroConditionVideo::GetScreenshot()
+{
+	auto source = obs_weak_source_get_source(_videoSource);
+	_screenshotData = std::make_unique<AdvSSScreenshotObj>(source);
+	obs_source_release(source);
+}
+
+bool MacroConditionVideo::LoadImageFromFile()
+{
+	if (!_matchImage.load(QString::fromStdString(_file))) {
+		blog(LOG_WARNING, "Cannot load image data from file '%s'",
+		     _file.c_str());
+		return false;
+	}
+	_matchImage =
+		_matchImage.convertToFormat(QImage::Format::Format_RGBX8888);
+	return true;
+}
+
+bool MacroConditionVideo::Compare()
+{
+	switch (_condition) {
+	case VideoCondition::MATCH:
+		return _screenshotData->image == _matchImage;
+	case VideoCondition::DIFFER:
+		return _screenshotData->image != _matchImage;
+	case VideoCondition::HAS_CHANGED:
+		return _screenshotData->image != _matchImage;
+	case VideoCondition::HAS_NOT_CHANGED:
+		return _screenshotData->image == _matchImage;
+	case VideoCondition::NO_IMAGE:
+		return _screenshotData->image.isNull();
+	default:
+		break;
+	}
+	return false;
+}
+
+static inline void populateConditionSelection(QComboBox *list)
+{
+	for (auto entry : conditionTypes) {
+		list->addItem(obs_module_text(entry.second.c_str()));
+	}
+}
+
+MacroConditionVideoEdit::MacroConditionVideoEdit(
+	QWidget *parent, std::shared_ptr<MacroConditionVideo> entryData)
+	: QWidget(parent)
+{
+	_videoSelection = new QComboBox();
+	_condition = new QComboBox();
+	_duration = new DurationSelection();
+	_filePath = new QLineEdit();
+	_browseButton =
+		new QPushButton(obs_module_text("AdvSceneSwitcher.browse"));
+
+	_filePath->setFixedWidth(100);
+
+	_browseButton->setStyleSheet("border:1px solid gray;");
+
+	QWidget::connect(_videoSelection,
+			 SIGNAL(currentTextChanged(const QString &)), this,
+			 SLOT(SourceChanged(const QString &)));
+	QWidget::connect(_condition, SIGNAL(currentIndexChanged(int)), this,
+			 SLOT(ConditionChanged(int)));
+	QWidget::connect(_duration, SIGNAL(DurationChanged(double)), this,
+			 SLOT(DurationChanged(double)));
+	QWidget::connect(_duration, SIGNAL(UnitChanged(DurationUnit)), this,
+			 SLOT(DurationUnitChanged(DurationUnit)));
+	QWidget::connect(_filePath, SIGNAL(editingFinished()), this,
+			 SLOT(FilePathChanged()));
+	QWidget::connect(_browseButton, SIGNAL(clicked()), this,
+			 SLOT(BrowseButtonClicked()));
+
+	AdvSceneSwitcher::populateVideoSelection(_videoSelection);
+	populateConditionSelection(_condition);
+
+	QHBoxLayout *mainLayout = new QHBoxLayout;
+	std::unordered_map<std::string, QWidget *> widgetPlaceholders = {
+		{"{{videoSources}}", _videoSelection},
+		{"{{condition}}", _condition},
+		{"{{duration}}", _duration},
+		{"{{filePath}}", _filePath},
+		{"{{browseButton}}", _browseButton},
+	};
+	placeWidgets(
+		obs_module_text("AdvSceneSwitcher.macro.condition.video.entry"),
+		mainLayout, widgetPlaceholders);
+	setLayout(mainLayout);
+
+	_entryData = entryData;
+	UpdateEntryData();
+	_loading = false;
+}
+
+void MacroConditionVideoEdit::UpdatePreviewTooltip()
+{
+	if (!_entryData || !requiresFileInput(_entryData->_condition)) {
+		return;
+	}
+
+	QImage preview = _entryData->GetMatchImage().scaled(
+		{300, 300}, Qt::KeepAspectRatio);
+
+	QByteArray data;
+	QBuffer buffer(&data);
+	if (!preview.save(&buffer, "PNG")) {
+		return;
+	}
+
+	QString html =
+		QString("<html><img src='data:image/png;base64, %0'/></html>")
+			.arg(QString(data.toBase64()));
+	this->setToolTip(html);
+}
+
+void MacroConditionVideoEdit::SetFilePath(const QString &text)
+{
+	_filePath->setText(text);
+	FilePathChanged();
+}
+
+void MacroConditionVideoEdit::SourceChanged(const QString &text)
+{
+	if (_loading || !_entryData) {
+		return;
+	}
+
+	std::lock_guard<std::mutex> lock(switcher->m);
+	_entryData->_videoSource = GetWeakSourceByQString(text);
+}
+
+void MacroConditionVideoEdit::ConditionChanged(int cond)
+{
+	if (_loading || !_entryData) {
+		return;
+	}
+
+	std::lock_guard<std::mutex> lock(switcher->m);
+	_entryData->_condition = static_cast<VideoCondition>(cond);
+
+	if (requiresFileInput(_entryData->_condition)) {
+		_filePath->show();
+		_browseButton->show();
+	} else {
+		_filePath->hide();
+		_browseButton->hide();
+	}
+
+	// Reload image data to avoid incorrect matches.
+	//
+	// Condition type HAS_NOT_CHANGED will use matchImage to store previous
+	// frame of video source, which will differ from the image stored at
+	// specified file location.
+	if (_entryData->LoadImageFromFile()) {
+		UpdatePreviewTooltip();
+	}
+}
+
+void MacroConditionVideoEdit::FilePathChanged()
+{
+	if (_loading || !_entryData) {
+		return;
+	}
+
+	std::lock_guard<std::mutex> lock(switcher->m);
+	_entryData->_file = _filePath->text().toUtf8().constData();
+	if (_entryData->LoadImageFromFile()) {
+		UpdatePreviewTooltip();
+	}
+}
+
+void MacroConditionVideoEdit::BrowseButtonClicked()
+{
+	if (_loading || !_entryData) {
+		return;
+	}
+
+	QString path;
+	bool useExistingFile = false;
+	// Ask whether to create screenshot or to select existing file
+	if (_entryData->_videoSource) {
+		QMessageBox msgBox(
+			QMessageBox::Question,
+			obs_module_text("AdvSceneSwitcher.windowTitle"),
+			obs_module_text(
+				"AdvSceneSwitcher.condition.video.askFileAction"),
+			QMessageBox::Yes | QMessageBox::No);
+		msgBox.setWindowFlags(Qt::Window | Qt::WindowTitleHint |
+				      Qt::CustomizeWindowHint);
+		msgBox.setButtonText(
+			QMessageBox::Yes,
+			obs_module_text(
+				"AdvSceneSwitcher.condition.video.askFileAction.file"));
+		msgBox.setButtonText(
+			QMessageBox::No,
+			obs_module_text(
+				"AdvSceneSwitcher.condition.video.askFileAction.screenshot"));
+		useExistingFile = msgBox.exec() == QMessageBox::Yes;
+	}
+
+	if (useExistingFile) {
+		path = QFileDialog::getOpenFileName(this);
+		if (path.isEmpty()) {
+			return;
+		}
+
+	} else {
+		auto source =
+			obs_weak_source_get_source(_entryData->_videoSource);
+		auto screenshot = std::make_unique<AdvSSScreenshotObj>(source);
+		obs_source_release(source);
+
+		path = QFileDialog::getSaveFileName(this);
+		if (path.isEmpty()) {
+			return;
+		}
+		QFile file(path);
+		if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+			return;
+		}
+		if (!screenshot->done) { // Screenshot usually completed by now
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+		}
+		if (!screenshot->done) {
+			DisplayMessage("Failed to get screenshot of source!");
+			return;
+		}
+
+		screenshot->image.save(path);
+	}
+	SetFilePath(path);
+}
+
+void MacroConditionVideoEdit::DurationChanged(double seconds)
+{
+	if (_loading || !_entryData) {
+		return;
+	}
+
+	std::lock_guard<std::mutex> lock(switcher->m);
+	_entryData->_duration.seconds = seconds;
+}
+
+void MacroConditionVideoEdit::DurationUnitChanged(DurationUnit unit)
+{
+	if (_loading || !_entryData) {
+		return;
+	}
+
+	std::lock_guard<std::mutex> lock(switcher->m);
+	_entryData->_duration.displayUnit = unit;
+}
+
+void MacroConditionVideoEdit::UpdateEntryData()
+{
+	if (!_entryData) {
+		return;
+	}
+
+	_videoSelection->setCurrentText(
+		GetWeakSourceName(_entryData->_videoSource).c_str());
+	_condition->setCurrentIndex(static_cast<int>(_entryData->_condition));
+	_duration->SetDuration(_entryData->_duration);
+	_filePath->setText(QString::fromStdString(_entryData->_file));
+
+	if (!requiresFileInput(_entryData->_condition)) {
+		_filePath->hide();
+		_browseButton->hide();
+	}
+}
