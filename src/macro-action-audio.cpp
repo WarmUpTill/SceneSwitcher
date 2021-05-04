5a250af247dbf4f49601d6e755f7fa26523e7c72
WarmUpTill <WarmUpTill@web.de>
2021-05-04 20:26:01 +0200

    Add macro action 'audio'
    For now this allow you to un-/mute audio sources, but it could be
    expanded in future to allow more complex audio operations

---
+++ src/macro-action-audio.cpp
@@ -0,0 +1,119 @@
+#include "headers/macro-action-audio.hpp"
+#include "headers/advanced-scene-switcher.hpp"
+#include "headers/utility.hpp"
+
+const int MacroActionAudio::id = 2;
+
+bool MacroActionAudio::_registered = MacroActionFactory::Register(
+	MacroActionAudio::id,
+	{MacroActionAudio::Create, MacroActionAudioEdit::Create,
+	 "AdvSceneSwitcher.action.audio"});
+
+static std::unordered_map<AudioAction, std::string> actionTypes = {
+	{AudioAction::MUTE, "AdvSceneSwitcher.action.audio.type.mute"},
+	{AudioAction::UNMUTE, "AdvSceneSwitcher.action.audio.type.unmute"},
+};
+
+bool MacroActionAudio::PerformAction()
+{
+	auto s = obs_weak_source_get_source(_audioSource);
+	switch (_action) {
+	case AudioAction::MUTE:
+		obs_source_set_muted(s, true);
+		break;
+	case AudioAction::UNMUTE:
+		obs_source_set_muted(s, false);
+		break;
+	default:
+		break;
+	}
+	obs_source_release(s);
+	return true;
+}
+
+bool MacroActionAudio::Save(obs_data_t *obj)
+{
+	MacroAction::Save(obj);
+	obs_data_set_string(obj, "audioSource",
+			    GetWeakSourceName(_audioSource).c_str());
+	obs_data_set_int(obj, "action", static_cast<int>(_action));
+	return true;
+}
+
+bool MacroActionAudio::Load(obs_data_t *obj)
+{
+	MacroAction::Load(obj);
+	const char *audioSourceName = obs_data_get_string(obj, "audioSource");
+	_audioSource = GetWeakSourceByName(audioSourceName);
+	_action = static_cast<AudioAction>(obs_data_get_int(obj, "action"));
+	return true;
+}
+
+static inline void populateActionSelection(QComboBox *list)
+{
+	for (auto entry : actionTypes) {
+		list->addItem(obs_module_text(entry.second.c_str()));
+	}
+}
+
+MacroActionAudioEdit::MacroActionAudioEdit(
+	QWidget *parent, std::shared_ptr<MacroActionAudio> entryData)
+	: QWidget(parent)
+{
+	_audioSources = new QComboBox();
+	_actions = new QComboBox();
+
+	populateActionSelection(_actions);
+	AdvSceneSwitcher::populateAudioSelection(_audioSources);
+
+	QWidget::connect(_actions, SIGNAL(currentIndexChanged(int)), this,
+			 SLOT(ActionChanged(int)));
+	QWidget::connect(_audioSources,
+			 SIGNAL(currentTextChanged(const QString &)), this,
+			 SLOT(SourceChanged(const QString &)));
+
+	QHBoxLayout *mainLayout = new QHBoxLayout;
+	std::unordered_map<std::string, QWidget *> widgetPlaceholders = {
+		{"{{audioSources}}", _audioSources},
+		{"{{actions}}", _actions},
+	};
+	placeWidgets(
+		obs_module_text("AdvSceneSwitcher.macro.action.audio.entry"),
+		mainLayout, widgetPlaceholders);
+	setLayout(mainLayout);
+
+	_entryData = entryData;
+	UpdateEntryData();
+	_loading = false;
+}
+
+void MacroActionAudioEdit::UpdateEntryData()
+{
+	if (!_entryData) {
+		return;
+	}
+
+	_audioSources->setCurrentText(
+		GetWeakSourceName(_entryData->_audioSource).c_str());
+	_actions->setCurrentIndex(static_cast<int>(_entryData->_action));
+}
+
+void MacroActionAudioEdit::SourceChanged(const QString &text)
+{
+	if (_loading || !_entryData) {
+		return;
+	}
+
+	std::lock_guard<std::mutex> lock(switcher->m);
+	_entryData->_audioSource = GetWeakSourceByQString(text);
+}
+
+void MacroActionAudioEdit::ActionChanged(int value)
+{
+	if (_loading || !_entryData) {
+		return;
+	}
+
+	std::lock_guard<std::mutex> lock(switcher->m);
+	_entryData->_action = static_cast<AudioAction>(value);
+}
