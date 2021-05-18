dde0046edf2463b138946ac857586bf190356048
WarmUpTill <WarmUpTill@web.de>
2021-05-18 19:11:23 +0200

    Add macro action "source"

---
+++ src/headers/macro-action-source.hpp
@@ -0,0 +1,58 @@
+#pragma once
+#include <QSpinBox>
+#include "macro-action-edit.hpp"
+
+enum class SourceAction {
+	ENABLE,
+	DISABLE,
+};
+
+class MacroActionSource : public MacroAction {
+public:
+	bool PerformAction();
+	void LogAction();
+	bool Save(obs_data_t *obj);
+	bool Load(obs_data_t *obj);
+	int GetId() { return id; };
+	static std::shared_ptr<MacroAction> Create()
+	{
+		return std::make_shared<MacroActionSource>();
+	}
+
+	OBSWeakSource _source;
+	SourceAction _action = SourceAction::ENABLE;
+
+private:
+	static bool _registered;
+	static const int id;
+};
+
+class MacroActionSourceEdit : public QWidget {
+	Q_OBJECT
+
+public:
+	MacroActionSourceEdit(
+		QWidget *parent,
+		std::shared_ptr<MacroActionSource> entryData = nullptr);
+	void UpdateEntryData();
+	static QWidget *Create(QWidget *parent,
+			       std::shared_ptr<MacroAction> action)
+	{
+		return new MacroActionSourceEdit(
+			parent,
+			std::dynamic_pointer_cast<MacroActionSource>(action));
+	}
+
+private slots:
+	void SourceChanged(const QString &text);
+	void ActionChanged(int value);
+
+protected:
+	QComboBox *_sources;
+	QComboBox *_actions;
+	std::shared_ptr<MacroActionSource> _entryData;
+
+private:
+	QHBoxLayout *_mainLayout;
+	bool _loading = true;
+};
