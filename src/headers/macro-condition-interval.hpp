2ac3477c52d69291398c1c41307e1d055e23e3fe
WarmUpTill <WarmUpTill@web.de>
2021-05-19 20:16:50 +0200

    Add macro condition "interval"

---
+++ src/headers/macro-condition-interval.hpp
@@ -0,0 +1,52 @@
+#pragma once
+#include "macro.hpp"
+#include <QWidget>
+#include <QComboBox>
+#include "duration-control.hpp"
+
+class MacroConditionInterval : public MacroCondition {
+public:
+	bool CheckCondition();
+	bool Save(obs_data_t *obj);
+	bool Load(obs_data_t *obj);
+	int GetId() { return id; };
+	static std::shared_ptr<MacroCondition> Create()
+	{
+		return std::make_shared<MacroConditionInterval>();
+	}
+
+	Duration _duration;
+
+private:
+	static bool _registered;
+	static const int id;
+};
+
+class MacroConditionIntervalEdit : public QWidget {
+	Q_OBJECT
+
+public:
+	MacroConditionIntervalEdit(
+		QWidget *parent,
+		std::shared_ptr<MacroConditionInterval> cond = nullptr);
+	void UpdateEntryData();
+	static QWidget *Create(QWidget *parent,
+			       std::shared_ptr<MacroCondition> cond)
+	{
+		return new MacroConditionIntervalEdit(
+			parent,
+			std::dynamic_pointer_cast<MacroConditionInterval>(
+				cond));
+	}
+
+private slots:
+	void DurationChanged(double seconds);
+	void DurationUnitChanged(DurationUnit unit);
+
+protected:
+	DurationSelection *_duration;
+	std::shared_ptr<MacroConditionInterval> _entryData;
+
+private:
+	bool _loading = true;
+};
