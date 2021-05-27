aa7714210a2da7510292585ed4507460976da587
WarmUpTill <WarmUpTill@web.de>
2021-05-27 20:55:31 +0200

    Rework section widget to support dynamically sized content

--- src/headers/section.hpp
+++ src/headers/section.hpp
@@ -14,18 +14,30 @@ public:
 	explicit Section(const int animationDuration = 300,
 			 QWidget *parent = 0);
 
-	void SetContent(QWidget *w);
+	void SetContent(QWidget *w, bool collapsed = true);
 	void AddHeaderWidget(QWidget *);
 
-public slots:
-	void Collapse(bool collapsed);
+protected:
+	bool eventFilter(QObject *obj, QEvent *event) override;
+
+private slots:
+	void AnimationFinished();
+	void Collapse(bool collapse);
 
 private:
+	void SetupAnimations();
+	void CleanUpPreviousContent();
+
 	QGridLayout *_mainLayout;
 	QHBoxLayout *_headerWidgetLayout;
 	QToolButton *_toggleButton;
 	QFrame *_headerLine;
 	QParallelAnimationGroup *_toggleAnimation = nullptr;
+	QParallelAnimationGroup *_contentAnimation = nullptr;
 	QScrollArea *_contentArea = nullptr;
+	QWidget *_content = nullptr;
 	int _animationDuration;
+	std::atomic_bool _transitioning = {false};
+	int _headerHeight = 0;
+	int _contentHeight = 0;
 };
