9cabc345a8d79c7fd7d2cfcbf63b0964ce56306f
WarmUpTill <WarmUpTill@web.de>
2021-06-01 21:09:53 +0200

    Add helper getDataFilePath()
    Used to get absolute file path to files in the plugin's data directory

--- src/utility.cpp
+++ src/utility.cpp
@@ -218,6 +218,17 @@ std::string getSourceSettings(OBSWeakSource ws)
 	return settings;
 }
 
+std::filesystem::path getDataFilePath(const std::string &file)
+{
+	const char *root_path = obs_get_module_data_path(obs_current_module());
+	if (root_path) {
+		auto ret = std::filesystem::u8path(root_path);
+		ret.append(file.data());
+		return ret;
+	}
+	return "";
+}
+
 bool DisplayMessage(const QString &msg, bool question)
 {
 	if (question) {
