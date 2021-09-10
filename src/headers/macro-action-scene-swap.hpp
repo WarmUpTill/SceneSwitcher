a002d3d4b5e9badc49c8521480f60f16a36c7f91
WarmUpTill <WarmUpTill@web.de>
2021-09-10 22:20:21 +0200

    Add macro action scene swap

---
+++ src/headers/macro-action-scene-swap.hpp
@@ -0,0 +1,38 @@
+#pragma once
+#include "macro-action-edit.hpp"
+
+class MacroActionSceneSwap : public MacroAction {
+public:
+	bool PerformAction();
+	void LogAction();
+	bool Save(obs_data_t *obj);
+	bool Load(obs_data_t *obj);
+	std::string GetId() { return id; };
+	static std::shared_ptr<MacroAction> Create()
+	{
+		return std::make_shared<MacroActionSceneSwap>();
+	}
+
+private:
+	static bool _registered;
+	static const std::string id;
+};
+
+class MacroActionSceneSwapEdit : public QWidget {
+	Q_OBJECT
+
+public:
+	MacroActionSceneSwapEdit(
+		QWidget *parent,
+		std::shared_ptr<MacroActionSceneSwap> entryData = nullptr);
+	static QWidget *Create(QWidget *parent,
+			       std::shared_ptr<MacroAction> action)
+	{
+		return new MacroActionSceneSwapEdit(
+			parent, std::dynamic_pointer_cast<MacroActionSceneSwap>(
+					action));
+	}
+
+protected:
+	std::shared_ptr<MacroActionSceneSwap> _entryData;
+};
