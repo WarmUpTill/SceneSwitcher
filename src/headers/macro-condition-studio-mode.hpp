15eeec8ebf5e1da73de89e7de030b622e5e662f7
WarmUpTill <WarmUpTill@web.de>
2021-12-17 21:16:37 +0100

    Add condition "Studio Mode"
    Can be used to check whether or not studio mode is active or to check
    the current preview scene

---
+++ src/headers/macro-condition-studio-mode.hpp
@@ -0,0 +1,65 @@
+#pragma once
+#include "macro.hpp"
+#include "scene-selection.hpp"
+
+#include <QWidget>
+#include <QComboBox>
+
+enum class StudioModeCondition {
+	STUDIO_MODE_ACTIVE,
+	STUDIO_MODE_NOT_ACTIVE,
+	PREVIEW_SCENE,
+};
+
+class MacroConditionStudioMode : public MacroCondition {
+public:
+	bool CheckCondition();
+	bool Save(obs_data_t *obj);
+	bool Load(obs_data_t *obj);
+	std::string GetShortDesc();
+	std::string GetId() { return id; };
+	static std::shared_ptr<MacroCondition> Create()
+	{
+		return std::make_shared<MacroConditionStudioMode>();
+	}
+
+	StudioModeCondition _condition =
+		StudioModeCondition::STUDIO_MODE_ACTIVE;
+	SceneSelection _scene;
+
+private:
+	static bool _registered;
+	static const std::string id;
+};
+
+class MacroConditionStudioModeEdit : public QWidget {
+	Q_OBJECT
+
+public:
+	MacroConditionStudioModeEdit(
+		QWidget *parent,
+		std::shared_ptr<MacroConditionStudioMode> cond = nullptr);
+	void UpdateEntryData();
+	static QWidget *Create(QWidget *parent,
+			       std::shared_ptr<MacroCondition> cond)
+	{
+		return new MacroConditionStudioModeEdit(
+			parent,
+			std::dynamic_pointer_cast<MacroConditionStudioMode>(
+				cond));
+	}
+private slots:
+	void ConditionChanged(int cond);
+	void SceneChanged(const SceneSelection &);
+signals:
+	void HeaderInfoChanged(const QString &);
+
+protected:
+	QComboBox *_condition;
+	SceneSelectionWidget *_scenes;
+	std::shared_ptr<MacroConditionStudioMode> _entryData;
+
+private:
+	void SetWidgetVisibility();
+	bool _loading = true;
+};
