2a56cc5e9ec55953641bd1cb6afa845a87af8048
WarmUpTill <WarmUpTill@web.de>
2022-06-23 19:45:34 +0200

    Add condition to check current profile

---
+++ src/headers/macro-condition-profile.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include "macro.hpp"
+#include <QWidget>
+#include <QComboBox>
+
+class MacroConditionProfile : public MacroCondition {
+public:
+	MacroConditionProfile(Macro *m) : MacroCondition(m) {}
+	bool CheckCondition();
+	bool Save(obs_data_t *obj);
+	bool Load(obs_data_t *obj);
+	std::string GetShortDesc();
+	std::string GetId() { return id; };
+	static std::shared_ptr<MacroCondition> Create(Macro *m)
+	{
+		return std::make_shared<MacroConditionProfile>(m);
+	}
+
+	std::string _profile;
+
+private:
+	static bool _registered;
+	static const std::string id;
+};
+
+class MacroConditionProfileEdit : public QWidget {
+	Q_OBJECT
+
+public:
+	MacroConditionProfileEdit(
+		QWidget *parent,
+		std::shared_ptr<MacroConditionProfile> cond = nullptr);
+	void UpdateEntryData();
+	static QWidget *Create(QWidget *parent,
+			       std::shared_ptr<MacroCondition> cond)
+	{
+		return new MacroConditionProfileEdit(
+			parent,
+			std::dynamic_pointer_cast<MacroConditionProfile>(cond));
+	}
+
+private slots:
+	void ProfileChanged(const QString &text);
+signals:
+	void HeaderInfoChanged(const QString &);
+
+protected:
+	QComboBox *_profiles;
+	std::shared_ptr<MacroConditionProfile> _entryData;
+
+private:
+	bool _loading = true;
+};
