6f7bcc34af87519546204eea70e736f2e660986f
WarmUpTill <WarmUpTill@web.de>
2022-08-21 04:09:31 +0200

    Add helper functions to save / load list of macro refs

--- src/macro-core/macro-ref.hpp
+++ src/macro-core/macro-ref.hpp
@@ -12,7 +12,7 @@ public:
 	void UpdateRef();
 	void UpdateRef(std::string name);
 	void UpdateRef(QString name);
-	void Save(obs_data_t *obj);
+	void Save(obs_data_t *obj) const;
 	void Load(obs_data_t *obj);
 	Macro *get();
 	Macro *operator->();
@@ -21,3 +21,8 @@ private:
 	std::string _name = "";
 	Macro *_ref = nullptr;
 };
+
+void SaveMacroList(obs_data_t *obj, const std::vector<MacroRef> &macros,
+		   const std::string &name = "macros");
+void LoadMacroList(obs_data_t *obj, std::vector<MacroRef> &macros,
+		   const std::string &name = "macros");
