d094c189d32dd5ca322a16ecc67b2e49a2746462
WarmUpTill <WarmUpTill@web.de>
2023-01-28 00:47:33 +0100

    Handle invalid paths due to scene collection name in settings export

--- src/general.cpp
+++ src/general.cpp
@@ -255,11 +255,28 @@ QString getDefaultSaveLocation()
 {
 	QString desktopPath = QStandardPaths::writableLocation(
 		QStandardPaths::DesktopLocation);
+
 	auto scName = obs_frontend_get_current_scene_collection();
 	QString sceneCollectionName(scName);
 	bfree(scName);
+
 	auto timestamp = QDateTime::currentDateTime();
-	return desktopPath + "/adv-ss-" + sceneCollectionName + "-" +
+	auto path = desktopPath + "/adv-ss-" + sceneCollectionName + "-" +
+		    timestamp.toString("yyyy.MM.dd.hh.mm.ss");
+
+	// Check if scene collection name contains invalid path characters
+	QFile file(path);
+	if (file.exists()) {
+		return path;
+	}
+
+	bool validPath = file.open(QIODevice::WriteOnly);
+	if (validPath) {
+		file.remove();
+		return path;
+	}
+
+	return desktopPath + "/adv-ss-" +
 	       timestamp.toString("yyyy.MM.dd.hh.mm.ss");
 }
 
