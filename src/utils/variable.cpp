2c16ad557ddcbfc8c708f954d55878f7fe24a022
WarmUpTill <WarmUpTill@web.de>
2023-02-22 15:03:48 +0100

    Move replaceAll() to utility

--- src/utils/variable.cpp
+++ src/utils/variable.cpp
@@ -1,4 +1,5 @@
 #include "variable.hpp"
+#include "utility.hpp"
 
 #include <switcher-data-structs.hpp>
 
@@ -158,19 +159,6 @@ const char *VariableResolvingString::c_str() const
 {
 	// Just assume that the value was previously resolved already
 	return _resolvedValue.c_str();
-}
-
-static void replaceAll(std::string &str, const std::string &from,
-		       const std::string &to)
-{
-	if (from.empty()) {
-		return;
-	}
-	size_t start_pos = 0;
-	while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
-		str.replace(start_pos, from.length(), to);
-		start_pos += to.length();
-	}
 }
 
 std::string SubstitueVariables(std::string str)
