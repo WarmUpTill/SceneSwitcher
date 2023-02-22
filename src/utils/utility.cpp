0bf70e443ee125aeb836706a8e3e223c595fa052
WarmUpTill <WarmUpTill@web.de>
2023-02-22 15:03:48 +0100

    Move replaceAll() to utility

--- src/utils/utility.cpp
+++ src/utils/utility.cpp
@@ -1042,3 +1042,16 @@ std::pair<int, int> getCursorPos()
 	auto cursorPos = QCursor::pos();
 	return {cursorPos.x(), cursorPos.y()};
 }
+
+void replaceAll(std::string &str, const std::string &from,
+		const std::string &to)
+{
+	if (from.empty()) {
+		return;
+	}
+	size_t start_pos = 0;
+	while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+		str.replace(start_pos, from.length(), to);
+		start_pos += to.length();
+	}
+}
