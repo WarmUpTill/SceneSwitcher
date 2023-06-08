18e19fb6c8797eaefef60d068ed3e02043729e26
WarmUpTill <WarmUpTill@web.de>
2023-06-08 12:28:26 +0200

    Add support for adding status labels to macro docks

--- src/macro-core/macro-dock.hpp
+++ src/macro-core/macro-dock.hpp
@@ -3,6 +3,7 @@
 
 #include <QPushButton>
 #include <QTimer>
+#include <QLabel>
 #include <memory>
 
 namespace advss {
@@ -15,24 +16,33 @@ class MacroDock : public OBSDock {
 public:
 	MacroDock(Macro *, QWidget *parent, const QString &runButtonText,
 		  const QString &pauseButtonText,
-		  const QString &unpauseButtonText);
+		  const QString &unpauseButtonText,
+		  const QString &conditionsTrueText,
+		  const QString &conditionsFalseText);
 	void SetName(const QString &);
 	void ShowRunButton(bool);
 	void SetRunButtonText(const QString &);
 	void ShowPauseButton(bool);
 	void SetPauseButtonText(const QString &);
 	void SetUnpauseButtonText(const QString &);
+	void ShowStatusLabel(bool);
+	void SetConditionsTrueText(const QString &);
+	void SetConditionsFalseText(const QString &);
 
 private slots:
 	void RunClicked();
 	void PauseToggleClicked();
 	void UpdatePauseText();
+	void UpdateStatusText();
 
 private:
 	QString _pauseButtonText;
 	QString _unpauseButtonText;
+	QString _conditionsTrueText;
+	QString _conditionsFalseText;
 	QPushButton *_run;
 	QPushButton *_pauseToggle;
+	QLabel *_statusText;
 
 	QTimer _timer;
 
