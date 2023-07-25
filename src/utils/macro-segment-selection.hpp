9eef581161ea755b7a7737a70c0566aab7ccbeb1
WarmUpTill <WarmUpTill@web.de>
2023-07-25 21:24:23 +0200

    Add MacroSegmentSelection
    Utility class to enable selection of index of a macro segments.
    Add description of selected segment.
    Highlights segment if selected macro is current macro.

---
+++ src/utils/macro-segment-selection.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include "variable-spinbox.hpp"
+
+namespace advss {
+
+class Macro;
+
+class MacroSegmentSelection : public QWidget {
+	Q_OBJECT
+public:
+	enum class Type { CONDITION, ACTION };
+
+	MacroSegmentSelection(QWidget *parent, Type type,
+			      bool allowVariables = true);
+	void SetMacro(const std::shared_ptr<Macro> &macro);
+	void SetMacro(Macro *macro);
+	void SetValue(const IntVariable &value);
+	void SetType(const Type &value);
+
+private slots:
+	void IndexChanged(const NumberVariable<int> &value);
+	void MacroSegmentOrderChanged();
+signals:
+	void SelectionChanged(const IntVariable &value);
+
+private:
+	void SetupDescription() const;
+	void MarkSelectedSegment();
+
+	VariableSpinBox *_index;
+	QLabel *_description;
+
+	Type _type;
+	Macro *_macro = nullptr;
+};
+
+} // namespace advss
