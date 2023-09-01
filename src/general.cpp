84cc4787f203d84ee521b6a3a0064d95c1b3932c
WarmUpTill <WarmUpTill@web.de>
2023-09-01 20:18:40 +0200

    Move splitter save/load helpers to utility

--- src/general.cpp
+++ src/general.cpp
@@ -676,34 +676,6 @@ void SwitcherData::LoadGeneralSettings(obs_data_t *obj)
 	lastImportPath = obs_data_get_string(obj, "lastImportPath");
 }
 
-static void saveSplitterPos(QList<int> &sizes, obs_data_t *obj,
-			    const std::string name)
-{
-	auto array = obs_data_array_create();
-	for (int i = 0; i < sizes.count(); ++i) {
-		obs_data_t *array_obj = obs_data_create();
-		obs_data_set_int(array_obj, "pos", sizes[i]);
-		obs_data_array_push_back(array, array_obj);
-		obs_data_release(array_obj);
-	}
-	obs_data_set_array(obj, name.c_str(), array);
-	obs_data_array_release(array);
-}
-
-static void loadSplitterPos(QList<int> &sizes, obs_data_t *obj,
-			    const std::string name)
-{
-	sizes.clear();
-	obs_data_array_t *array = obs_data_get_array(obj, name.c_str());
-	size_t count = obs_data_array_count(array);
-	for (size_t i = 0; i < count; i++) {
-		obs_data_t *item = obs_data_array_item(array, i);
-		sizes << obs_data_get_int(item, "pos");
-		obs_data_release(item);
-	}
-	obs_data_array_release(array);
-}
-
 void SwitcherData::SaveUISettings(obs_data_t *obj)
 {
 	obs_data_set_int(obj, "generalTabPos", tabOrder[0]);
