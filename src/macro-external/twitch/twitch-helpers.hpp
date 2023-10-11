8e13809017d6bb5a2062394ac52ad93f796993c5
WarmUpTill <WarmUpTill@web.de>
2023-10-11 12:26:18 +0200

    Add option to use cached results
    The cache will remain valid for 10s before it is refreshed with a "real"
    Get / Post / Patch call.

--- src/macro-external/twitch/twitch-helpers.hpp
+++ src/macro-external/twitch/twitch-helpers.hpp
@@ -2,6 +2,7 @@
 #include <httplib.h>
 #include <obs.hpp>
 #include <string>
+#include <chrono>
 
 namespace advss {
 
@@ -12,6 +13,8 @@ struct RequestResult {
 	OBSData data = nullptr;
 };
 
+// These functions do *not* use or create RequestResult cache entries
+
 RequestResult SendGetRequest(const std::string &uri, const std::string &path,
 			     const TwitchToken &token,
 			     const httplib::Params & = {});
@@ -19,6 +22,20 @@ RequestResult SendPostRequest(const std::string &uri, const std::string &path,
 			      const TwitchToken &token, const OBSData &data);
 RequestResult SendPatchRequest(const std::string &uri, const std::string &path,
 			       const TwitchToken &token, const OBSData &data);
+
+// These functions will cache the RequestResult for 10s
+// Note that the cache will be reported as a "memory leak" on OBS shutdown
+
+RequestResult SendGetRequest(const std::string &uri, const std::string &path,
+			     const TwitchToken &token, const httplib::Params &,
+			     bool useCache);
+RequestResult SendPostRequest(const std::string &uri, const std::string &path,
+			      const TwitchToken &token, const OBSData &data,
+			      bool useCache);
+RequestResult SendPatchRequest(const std::string &uri, const std::string &path,
+			       const TwitchToken &token, const OBSData &data,
+			       bool useCache);
+
 const char *GetClientID();
 
 } // namespace advss
