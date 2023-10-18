94060c236b9b42fd626f86b700985ec0f3d46b85
WarmUpTill <WarmUpTill@web.de>
2023-10-18 19:54:21 +0200

    Add option to set ResizingPlainTextEdit parameters

--- src/utils/variable-text-edit.hpp
+++ src/utils/variable-text-edit.hpp
@@ -7,7 +7,8 @@ namespace advss {
 class VariableTextEdit : public ResizingPlainTextEdit {
 	Q_OBJECT
 public:
-	VariableTextEdit(QWidget *parent);
+	VariableTextEdit(QWidget *parent, const int scrollAt = 10,
+			 const int minLines = 3, const int paddingLines = 2);
 	void setPlainText(const QString &);
 	void setPlainText(const StringVariable &);
 	void setToolTip(const QString &string);
