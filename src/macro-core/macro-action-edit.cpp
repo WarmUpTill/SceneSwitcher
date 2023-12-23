dcee98fea7f2ef6db30de095be774717cd88c96c
WarmUpTill <WarmUpTill@web.de>
2023-12-23 18:29:08 +0100

    Move macro segment factory functinos to seperate files

--- src/macro-core/macro-action-edit.cpp
+++ src/macro-core/macro-action-edit.cpp
@@ -1,6 +1,6 @@
+#include "macro-action-edit.hpp"
 #include "advanced-scene-switcher.hpp"
 #include "switcher-data.hpp"
-#include "macro-action-edit.hpp"
 #include "macro-action-scene-switch.hpp"
 #include "section.hpp"
 #include "switch-button.hpp"
@@ -10,61 +10,9 @@
 
 namespace advss {
 
-std::map<std::string, MacroActionInfo> &MacroActionFactory::GetMap()
-{
-	static std::map<std::string, MacroActionInfo> _methods;
-	return _methods;
-}
-
-bool MacroActionFactory::Register(const std::string &id, MacroActionInfo info)
-{
-	if (auto it = GetMap().find(id); it == GetMap().end()) {
-		GetMap()[id] = info;
-		return true;
-	}
-	return false;
-}
-
-std::shared_ptr<MacroAction> MacroActionFactory::Create(const std::string &id,
-							Macro *m)
-{
-	if (auto it = GetMap().find(id); it != GetMap().end())
-		return it->second._createFunc(m);
-
-	return nullptr;
-}
-
-QWidget *MacroActionFactory::CreateWidget(const std::string &id,
-					  QWidget *parent,
-					  std::shared_ptr<MacroAction> action)
-{
-	if (auto it = GetMap().find(id); it != GetMap().end())
-		return it->second._createWidgetFunc(parent, action);
-
-	return nullptr;
-}
-
-std::string MacroActionFactory::GetActionName(const std::string &id)
-{
-	if (auto it = GetMap().find(id); it != GetMap().end()) {
-		return it->second._name;
-	}
-	return "unknown action";
-}
-
-std::string MacroActionFactory::GetIdByName(const QString &name)
-{
-	for (auto it : GetMap()) {
-		if (name == obs_module_text(it.second._name.c_str())) {
-			return it.first;
-		}
-	}
-	return "";
-}
-
 static inline void populateActionSelection(QComboBox *list)
 {
-	for (auto &[_, action] : MacroActionFactory::GetActionTypes()) {
+	for (const auto &[_, action] : MacroActionFactory::GetActionTypes()) {
 		QString entry(obs_module_text(action._name.c_str()));
 		if (list->findText(entry) == -1) {
 			list->addItem(entry);
--- src/macro-core/macro-condition-edit.hpp
+++ src/macro-core/macro-condition-edit.hpp
@@ -1,37 +1,11 @@
 #pragma once
 #include "macro-condition.hpp"
+#include "macro-condition-factory.hpp"
 #include "filter-combo-box.hpp"
 
 #include <memory>
 
 namespace advss {
-
-struct MacroConditionInfo {
-	using TCreateMethod = std::shared_ptr<MacroCondition> (*)(Macro *m);
-	using TCreateWidgetMethod =
-		QWidget *(*)(QWidget *parent, std::shared_ptr<MacroCondition>);
-	TCreateMethod _createFunc = nullptr;
-	TCreateWidgetMethod _createWidgetFunc = nullptr;
-	std::string _name;
-	bool _useDurationModifier = true;
-};
-
-class MacroConditionFactory {
-public:
-	MacroConditionFactory() = delete;
-	static bool Register(const std::string &, MacroConditionInfo);
-	static std::shared_ptr<MacroCondition> Create(const std::string &,
-						      Macro *m);
-	static QWidget *CreateWidget(const std::string &id, QWidget *parent,
-				     std::shared_ptr<MacroCondition>);
-	static auto GetConditionTypes() { return GetMap(); }
-	static std::string GetConditionName(const std::string &);
-	static std::string GetIdByName(const QString &name);
-	static bool UsesDurationModifier(const std::string &id);
-
-private:
-	static std::map<std::string, MacroConditionInfo> &GetMap();
-};
 
 class DurationModifierEdit : public QWidget {
 	Q_OBJECT
