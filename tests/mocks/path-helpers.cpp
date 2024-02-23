89e14a0eebcc01b1e6ece9922844022bfc8d91e3
WarmUpTill <WarmUpTill@web.de>
2024-02-23 23:13:29 +0100

    Add more tests

---
+++ tests/mocks/path-helpers.cpp
@@ -0,0 +1,15 @@
+#include "path-helpers.hpp"
+
+namespace advss {
+
+std::string GetDataFilePath(const std::string &file)
+{
+	return "";
+}
+
+QString GetDefaultSettingsSaveLocation()
+{
+	return "";
+}
+
+} // namespace advss
