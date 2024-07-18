0f2bb02f3f7acd7cc65c13be6533b9e9e409f9c3
WarmUpTill <warmuptill@web.de>
2024-07-18 14:09:30 +0200

    lua

--- example-scripts/example_actions.lua
+++ example-scripts/example_actions.lua
@@ -1,5 +1,6 @@
 obs           = obslua
 
+-- Example actions
 function my_lua_action(data)
     obs.script_log(obs.LOG_WARNING, "hello from lua!")
 end
@@ -13,11 +14,16 @@ function blocking_lua_action_example(data)
     obs.script_log(obs.LOG_WARNING, "my blocking lua function is starting!")
     busy_sleep(3)
     obs.script_log(obs.LOG_WARNING, "my blocking lua function is finished!")
-    local signal_handler = obs.obs_get_signal_handler()
-    local signal_name = obs.calldata_string(data, "completion_signal_name")
-    obs.signal_handler_signal(signal_handler, signal_name, nil)
 end
 
+-- Register the example actions
+function script_load(settings)
+    register_action("My custom Lua Action", my_lua_action)
+    register_action("My blocking Lua Action", blocking_lua_action_example, true)
+end
+
+-- Advanced Scene Switcher helpers below
+-- Usually you should not have to modify this code and can copy it to your scripts as is
 function register_action(name, callback, blocking)
     local proc_handler = obs.obs_get_proc_handler()
     local data = obs.calldata_create()
@@ -33,17 +39,30 @@ function register_action(name, callback, blocking)
     local success = obs.calldata_bool(data, "success")
     if success == false then
         obs.script_log(obs.LOG_WARNING, string.format("failed to register custom action \"%s\"", name))
+        obs.calldata_destroy(data)
         return
     end
-    
+
+    -- Run action in coroutine to avoid blocking main OBS signal handler
+    -- Action completion will be indicated via signal completion_signal_name
+    local run_helper = (
+        function(data)
+            local completion_signal_name = obs.calldata_string(data, "completion_signal_name")
+            co = coroutine.create(
+                function(data)
+                    callback(data)
+                    local signal_handler = obs.obs_get_signal_handler()
+                    obs.signal_handler_signal(signal_handler, completion_signal_name, nil)
+                end
+            )
+            coroutine.resume(co)
+        end
+    )
+
     local signal_name = obs.calldata_string(data, "trigger_signal_name")
     local signal_handler = obs.obs_get_signal_handler()
-    obs.signal_handler_connect(signal_handler, signal_name, callback)
+    obs.signal_handler_connect(signal_handler, signal_name, run_helper)
 
     obs.calldata_destroy(data)
 end
 
-function script_load(settings)
-    register_action("My custom Lua Action", my_lua_action)
-    register_action("My blocking Lua Action", blocking_lua_action_example, true)
-end
