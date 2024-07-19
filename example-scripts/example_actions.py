9269ea5cab2ecfa44136a12f8eaf1878144ba67f
WarmUpTill <warmuptill@web.de>
2024-07-19 14:33:07 +0200

    draft python script with condition

--- example-scripts/example_actions.py
+++ example-scripts/example_actions.py
@@ -3,6 +3,7 @@ import threading
 import time
 
 counter = 0
+set_condition_value = None
 
 # Example actions
 def my_python_action(data):
@@ -28,13 +29,22 @@ def script_load(settings):
     advss_register_action("My blocking Python Action", blocking_python_action_example, True)
     advss_register_action("My variable Python Action", variable_python_action)
 
+    global set_condition_value
+    set_condition_value = advss_register_condition("My Python condition")
+    set_condition_value(True)
+
 def script_unload():
     advss_deregister_action("My custom Python Action")
     advss_deregister_action("My blocking Python Action")
     advss_deregister_action("My variable Python Action")
+    
+    advss_deregister_condition("My Python condition")
 
 # Advanced Scene Switcher helpers below
 # Usually you should not have to modify this code and can copy it to your scripts as is
+
+### Actions ###
+
 def advss_register_action(name, callback, blocking = False):
     proc_handler = obs.obs_get_proc_handler()
     data = obs.calldata_create()
@@ -79,6 +89,47 @@ def advss_deregister_action(name):
         obs.script_log(obs.LOG_WARNING, "failed to deregister custom action \"" + name + "\"")
     
     obs.calldata_destroy(data)
+
+### Conditions ###
+
+def advss_register_condition(name):
+    proc_handler = obs.obs_get_proc_handler()
+    data = obs.calldata_create()
+    
+    obs.calldata_set_string(data, "name", name)
+    obs.proc_handler_call(proc_handler, "advss_register_script_condition", data)
+    
+    success = obs.calldata_bool(data, "success")
+    if success == False:
+        obs.script_log(obs.LOG_WARNING, "failed to register custom condition \"" + name + "\"")
+        obs.calldata_destroy(data)
+        return
+    
+    signal_name = obs.calldata_string(data, "change_value_signal_name")
+    def set_condition_value_func(value):
+        data = obs.calldata_create()
+        obs.calldata_set_bool(data, "value", value)
+        signal_handler = obs.obs_get_signal_handler()
+        obs.signal_handler_signal(signal_handler, signal_name, None)
+        obs.calldata_destroy(data)
+
+    obs.calldata_destroy(data)
+    return set_condition_value_func
+
+def advss_deregister_condition(name):
+    proc_handler = obs.obs_get_proc_handler()
+    data = obs.calldata_create()
+    
+    obs.calldata_set_string(data, "name", name)
+    obs.proc_handler_call(proc_handler, "advss_deregister_script_condition", data)
+    
+    success = obs.calldata_bool(data, "success")
+    if success == False:
+        obs.script_log(obs.LOG_WARNING, "failed to deregister custom condition \"" + name + "\"")
+
+    obs.calldata_destroy(data)
+
+### Variables ###
 
 def advss_get_variable_value(name):
     proc_handler = obs.obs_get_proc_handler()
