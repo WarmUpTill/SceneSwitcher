dc6297bd12e6fac2e15fc18415229f53d689cca3
WarmUpTill <19472752+WarmUpTill@users.noreply.github.com>
2025-04-17 11:36:05 +0200

    Add more JSON helpers

--- lib/utils/json-helpers.hpp
+++ lib/utils/json-helpers.hpp
@@ -13,5 +13,9 @@ EXPORT bool MatchJson(const std::string &json1, const std::string &json2,
 		      const RegexConfig &regex);
 EXPORT std::optional<std::string> GetJsonField(const std::string &json,
 					       const std::string &id);
+EXPORT std::optional<std::string> QueryJson(const std::string &json,
+					    const std::string &query);
+EXPORT std::optional<std::string> AccessJsonArrayIndex(const std::string &json,
+						       const int index);
 
 } // namespace advss
