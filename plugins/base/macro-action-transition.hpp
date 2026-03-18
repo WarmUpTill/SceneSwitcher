81cc638f0fca6e2f6f21e9db58b81239f6947978
WarmUpTill <19472752+WarmUpTill@users.noreply.github.com>
2026-03-18 19:33:20 +0100

    Cleanup

--- plugins/base/macro-action-transition.hpp
+++ plugins/base/macro-action-transition.hpp
@@ -4,7 +4,6 @@
 #include "transition-selection.hpp"
 #include "scene-selection.hpp"
 #include "scene-item-selection.hpp"
-#include "variable-number.hpp"
 #include "variable-spinbox.hpp"
 
 #include <QCheckBox>
