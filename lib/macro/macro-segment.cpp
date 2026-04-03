WarmUpTill/SceneSwitcher

commit ec3052c823c23e30c4ec0323999e784ff7bcc9fe

Enable checking if temp var is in use

lib/macro/macro-segment.cpp
@@ -13,6 +13,11 @@
 
 namespace advss {
 
+std::vector<TempVariableRef> MacroSegment::GetTempVarRefs() const
+{
+	return {};
+}
+
 MacroSegment::MacroSegment(Macro *m, bool supportsVariableValue)
 	: _macro(m),
 	  _supportsVariableValue(supportsVariableValue)
@@ -150,6 +155,16 @@ void MacroSegment::AddTempvar(const std::string &id, const std::string &name,
 	TempVariable var(id, name, description, sharedSegment);
 	_tempVariables.emplace_back(std::move(var));
 	NotifyUIAboutTempVarChange(this);
+}
+
+bool MacroSegment::IsTempVarInUse(const std::string &id) const
+{
+	for (const auto &var : _tempVariables) {
+		if (var.ID() == id) {
+			return var.IsInUse();
+		}
+	}
+	return false;
 }
 
 void MacroSegment::SetTempVarValue(const std::string &id,

lib/macro/macro-segment.hpp
@@ -34,6 +34,7 @@ public:
 	std::string GetCustomLabel() const { return _customLabel; }
 	virtual bool Save(obs_data_t *obj) const = 0;
 	virtual bool Load(obs_data_t *obj) = 0;
+	virtual std::vector<TempVariableRef> GetTempVarRefs() const;
 	virtual bool PostLoad();
 	virtual std::string GetShortDesc() const;
 	virtual std::string GetId() const = 0;
@@ -54,6 +55,7 @@ protected:
 	void AddTempvar(const std::string &id, const std::string &name,
 			const std::string &description = "");
 
+	bool IsTempVarInUse(const std::string &id) const;
 	void SetTempVarValue(const std::string &id, const std::string &value);
 
 	template<typename T, typename = std::enable_if_t<
@@ -62,6 +64,14 @@ protected:
 	{
 		SetTempVarValue(id, value ? std::string("true")
 					  : std::string("false"));
+	}
+
+	template<typename F, typename = std::enable_if_t<std::is_invocable_v<F>>>
+	void SetTempVarValue(const std::string &id, F &&valueProvider)
+	{
+		if (IsTempVarInUse(id)) {
+			SetTempVarValue(id, valueProvider());
+		}
 	}
 
 private:

lib/macro/macro-action-variable.cpp
@@ -17,6 +17,14 @@ namespace advss {
 
 const std::string MacroActionVariable::id = "variable";
 
+std::vector<TempVariableRef> MacroActionVariable::GetTempVarRefs() const
+{
+	if (!_tempVar.HasValidID()) {
+		return {};
+	}
+	return {_tempVar};
+}
+
 bool MacroActionVariable::_registered = MacroActionFactory::Register(
 	MacroActionVariable::id,
 	{MacroActionVariable::Create, MacroActionVariableEdit::Create,
@@ -1450,6 +1458,7 @@ void MacroActionVariableEdit::SelectionChanged(const TempVariableRef &var)
 {
 	GUARD_LOADING_AND_LOCK();
 	_entryData->_tempVar = var;
+	IncrementTempVarInUseGeneration();
 	SetWidgetVisibility();
 }
 

lib/macro/macro-action-variable.hpp
@@ -23,6 +23,7 @@ public:
 	bool PostLoad();
 	std::string GetShortDesc() const;
 	std::string GetId() const { return id; };
+	std::vector<TempVariableRef> GetTempVarRefs() const;
 	static std::shared_ptr<MacroAction> Create(Macro *m);
 	std::shared_ptr<MacroAction> Copy() const;
 	void SetSegmentIndexValue(int);

lib/macro/macro-condition-tempvar.cpp
@@ -6,6 +6,14 @@ namespace advss {
 
 const std::string MacroConditionTempVar::id = "temp_var";
 
+std::vector<TempVariableRef> MacroConditionTempVar::GetTempVarRefs() const
+{
+	if (!_tempVar.HasValidID()) {
+		return {};
+	}
+	return {_tempVar};
+}
+
 bool MacroConditionTempVar::_registered = MacroConditionFactory::Register(
 	MacroConditionTempVar::id,
 	{MacroConditionTempVar::Create, MacroConditionTempVarEdit::Create,
@@ -266,6 +274,7 @@ void MacroConditionTempVarEdit::VariableChanged(const TempVariableRef &var)
 {
 	GUARD_LOADING_AND_LOCK();
 	_entryData->_tempVar = var;
+	IncrementTempVarInUseGeneration();
 }
 
 void MacroConditionTempVarEdit::Variable2Changed(const QString &text)

lib/macro/macro-condition-tempvar.hpp
@@ -20,6 +20,7 @@ public:
 	bool Load(obs_data_t *obj);
 	std::string GetShortDesc() const;
 	std::string GetId() const { return id; };
+	std::vector<TempVariableRef> GetTempVarRefs() const;
 	static std::shared_ptr<MacroCondition> Create(Macro *m)
 	{
 		return std::make_shared<MacroConditionTempVar>(m);

lib/utils/temp-variable.cpp
@@ -10,8 +10,11 @@
 
 #include <QVariant>
 #include <QAbstractItemView>
+#include <atomic>
 
 Q_DECLARE_METATYPE(advss::TempVariableRef);
+
+static std::atomic<uint64_t> tempVarInUseGeneration{0};
 
 namespace advss {
 
@@ -118,6 +121,60 @@ TempVariableRef TempVariable::GetRef() const
 	ref._id = _id;
 	ref._segment = _segment;
 	return ref;
+}
+
+static bool refsContain(const std::vector<TempVariableRef> &refs,
+			const TempVariableRef &ref)
+{
+	for (const auto &r : refs) {
+		if (r == ref) {
+			return true;
+		}
+	}
+	return false;
+}
+
+template<typename T>
+static bool segmentsReferTo(const T &segments, const TempVariableRef &ref)
+{
+	for (const auto &segment : segments) {
+		if (refsContain(segment->GetTempVarRefs(), ref)) {
+			return true;
+		}
+	}
+	return false;
+}
+
+bool TempVariable::IsInUse() const
+{
+	const auto currentGen =
+		tempVarInUseGeneration.load(std::memory_order_relaxed);
+	if (_isInUseCacheGeneration == currentGen) {
+		return _isInUseCache;
+	}
+
+	bool inUse = false;
+	const auto ref = GetRef();
+
+	if (ref.HasValidID()) {
+		for (const auto &macro : GetAllMacros()) {
+			if (segmentsReferTo(macro->Conditions(), ref) ||
+			    segmentsReferTo(macro->Actions(), ref) ||
+			    segmentsReferTo(macro->ElseActions(), ref)) {
+				inUse = true;
+				break;
+			}
+		}
+	}
+
+	_isInUseCache = inUse;
+	_isInUseCacheGeneration = currentGen;
+	return inUse;
+}
+
+void IncrementTempVarInUseGeneration()
+{
+	tempVarInUseGeneration.fetch_add(1, std::memory_order_relaxed);
 }
 
 TempVariableRef::SegmentType TempVariableRef::GetType() const
@@ -665,6 +722,7 @@ TempVarSignalManager *TempVarSignalManager::Instance()
 
 void NotifyUIAboutTempVarChange(MacroSegment *segment)
 {
+	IncrementTempVarInUseGeneration();
 	obs_queue_task(
 		OBS_TASK_UI,
 		[](void *segment) {

lib/utils/temp-variable.hpp
@@ -44,6 +44,7 @@ public:
 	void SetValue(const std::string &val);
 	void InvalidateValue();
 	TempVariableRef GetRef() const;
+	EXPORT bool IsInUse() const;
 
 private:
 	std::string _id = "";
@@ -53,6 +54,8 @@ private:
 	mutable std::mutex _lastValuesMutex;
 	std::vector<std::string> _lastValues;
 	bool _valueIsValid = false;
+	mutable bool _isInUseCache = false;
+	mutable uint64_t _isInUseCacheGeneration = UINT64_MAX;
 
 	std::weak_ptr<MacroSegment> _segment;
 	friend TempVariableSelection;
@@ -122,5 +125,6 @@ private:
 };
 
 void NotifyUIAboutTempVarChange(MacroSegment *);
+EXPORT void IncrementTempVarInUseGeneration();
 
 } // namespace advss

commit 36206a4830e2bfeab7bf0949da7b7f0dfc463f2f

Fix temp var values of actions not being accessible
This a bug introduced with ec3052c823c23e30c4ec0323999e784ff7bcc9fe.

lib/macro/macro-segment.cpp
@@ -167,6 +167,24 @@ bool MacroSegment::IsTempVarInUse(const std::string &id) const
 	return false;
 }
 
+void MacroSegment::CopyTempVarValuesFrom(const MacroSegment &other)
+{
+	for (const auto &src : other._tempVariables) {
+		for (auto &dst : _tempVariables) {
+			if (dst.ID() != src.ID()) {
+				continue;
+			}
+			auto value = src.Value();
+			if (value) {
+				dst.SetValue(*value);
+			} else {
+				dst.InvalidateValue();
+			}
+			break;
+		}
+	}
+}
+
 void MacroSegment::SetTempVarValue(const std::string &id,
 				   const std::string &value)
 {

lib/macro/macro-segment.hpp
@@ -57,6 +57,7 @@ protected:
 
 	bool IsTempVarInUse(const std::string &id) const;
 	void SetTempVarValue(const std::string &id, const std::string &value);
+	void CopyTempVarValuesFrom(const MacroSegment &other);
 
 	template<typename T, typename = std::enable_if_t<
 				     std::is_same<std::decay_t<T>, bool>::value>>

lib/macro/macro.cpp
@@ -433,6 +433,9 @@ bool Macro::RunActionsHelper(
 				actionCopy = action->Copy();
 			});
 			bool actionResult = actionCopy->PerformAction();
+			action->WithLock([&action, &actionCopy]() {
+				action->CopyTempVarValuesFrom(*actionCopy);
+			});
 			actionsExecutedSuccessfully =
 				actionsExecutedSuccessfully && actionResult;
 		} else {