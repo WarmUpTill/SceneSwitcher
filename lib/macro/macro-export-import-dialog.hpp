6fc819d72bb1c930868b531709d58b2c497327ad
WarmUpTill <19472752+WarmUpTill@users.noreply.github.com>
2026-04-19 19:54:00 +0200

    Add API to expand export / import dialog

--- lib/macro/macro-export-import-dialog.hpp
+++ lib/macro/macro-export-import-dialog.hpp
@@ -1,7 +1,13 @@
 #pragma once
+
+#include "section.hpp"
+
 #include <QCheckBox>
 #include <QDialog>
+#include <QList>
 #include <QPlainTextEdit>
+
+#include <obs-data.h>
 
 namespace advss {
 
@@ -9,17 +15,33 @@ class MacroExportImportDialog : public QDialog {
 	Q_OBJECT
 public:
 	enum class Type { EXPORT_MACRO, IMPORT_MACRO };
-	MacroExportImportDialog(Type type);
 
-	static void ExportMacros(const QString &json);
+	static void ExportMacros(obs_data_t *macroData);
 	static bool ImportMacros(QString &json);
 
 private slots:
 	void UsePlainTextChanged(int);
+	void UpdateExportString();
 
 private:
+	explicit MacroExportImportDialog(Type type,
+					 const QString &baseJson = {});
+
+	// Per-extension widgets
+	struct ExtensionUI {
+		QCheckBox *mainCheck = nullptr;
+		// (itemId, checkbox) pairs - empty when no per-item selection.
+		QList<QPair<QString, QCheckBox *>> itemChecks;
+	};
+
+	Section *BuildExtensionSection();
+	QString BuildExportJson() const;
+	void RefreshExportText();
+
+	QString _baseJson;
 	QPlainTextEdit *_importExportString;
 	QCheckBox *_usePlainText;
+	QList<ExtensionUI> _extensionUIs;
 };
 
 } // namespace advss
