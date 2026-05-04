92144eaf939e096fb3f9cb779b476936680661de
WarmUpTill <19472752+WarmUpTill@users.noreply.github.com>
2026-05-04 20:47:17 +0200

    Don't highlight action trigger mode when macro is paused

--- lib/macro/macro-tab.cpp
+++ lib/macro/macro-tab.cpp
@@ -550,6 +550,10 @@ void AdvSceneSwitcher::HighlightOnChange() const
 		return;
 	}
 
+	if (macro->Paused()) {
+		return;
+	}
+
 	if (macro->ActionTriggerModePreventedActionsSince(
 		    lastOnChangeHighlightCheckTime)) {
 		HighlightWidget(ui->actionTriggerMode, Qt::yellow,
